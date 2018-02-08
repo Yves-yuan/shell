b910d4588cb6fe52fb1df3608b164412c5fd7217
yuanyifei <yuanp0813@163.com>
2018-02-08 16:45:45 +0800

    1

---
+++ scp.sh
@@ -0,0 +1,7 @@
+src="D:\uidq1136\software\install-pkg/jdk-8u161-linux-x64.tar.gz"
+
+
+echo $src
+scp $src root@10.217.2.240:/home
+scp $src root@10.217.2.241:/home
+scp $src root@10.217.2.242:/home
