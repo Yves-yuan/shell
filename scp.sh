e4c401bb04d89105dee2863469699e0e2ca72820
yuanyifei <yuanp0813@163.com>
2018-04-10 16:43:46 +0800

    2

--- scp.sh
+++ scp.sh
@@ -1,9 +1,9 @@
-src="D:\uidq1136\software\develop\shell/scala-2.11.8.tgz"
+src="D:\uidq1136\software\develop\shell/driver_log4j.properties"
 array=(10.217.2.240 10.217.2.241 10.217.2.242)
 echo $src
 for i in ${array[@]};do 
 	echo $i
-	scp $src root@$i:/usr/local/software/scala
+	scp $src root@$i:/usr/local/software/sparkDemo
 done
 #scp $src root@10.217.2.240:/usr/local/software/scala
 #scp $src root@10.217.2.241:/usr/local/software/scala
