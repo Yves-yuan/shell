ec13f53fa9738fb0bdbc10703e4293fc55459e5e
yuanyifei <yuanp0813@163.com>
2018-04-10 14:31:06 +0800

    1

--- scp.sh
+++ scp.sh
@@ -1,7 +1,10 @@
-src="D:\uidq1136\software\green/spark-2.3.0-bin-hadoop2.7.tar"
-
-
+src="D:\uidq1136\software\develop\shell/scala-2.11.8.tgz"
+array=(10.217.2.240 10.217.2.241 10.217.2.242)
 echo $src
-scp $src root@10.217.2.240:/usr/local/software/spark
-scp $src root@10.217.2.241:/usr/local/software/spark
-scp $src root@10.217.2.242:/usr/local/software/spark
+for i in ${array[@]};do 
+	echo $i
+	scp $src root@$i:/usr/local/software/scala
+done
+#scp $src root@10.217.2.240:/usr/local/software/scala
+#scp $src root@10.217.2.241:/usr/local/software/scala
+#scp $src root@10.217.2.242:/usr/local/software/scala
